4020331e67b53c0f9580fa43c66150095a372de3
Yjack2701 <122147590+Yjack2701@users.noreply.github.com>
2023-01-25 16:22:32 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -7,19 +7,19 @@ download.file(url, file.path(path, "getdata_projectfiles_UCI HAR Dataset.zip"))
 unzip(zipfile = "getdata_projectfiles_UCI HAR Datasets.zip")
 
 #import and combines test and train X data
-temp_train <- read.table(file.path(path,"UCI HAR Dataset/train/X_train.txt"))
-temp_test <- read.table(file.path(path,"UCI HAR Dataset/test/X_test.txt"))
-X <- rbind(temp_train, temp_test)
+temp_1 <- read.table(file.path(path,"UCI HAR Dataset/train/X_train.txt"))
+temp_2 <- read.table(file.path(path,"UCI HAR Dataset/test/X_test.txt"))
+X_data <- rbind(temp_1, temp_2)
 
 #import and combines test and train Y data
-temp_train <- read.table(file.path(path,"UCI HAR Dataset/train/y_train.txt"))
-temp_test <- read.table(file.path(path,"UCI HAR Dataset/test/y_test.txt"))
-Y <- rbind(temp_train, temp_test)
+temp_1 <- read.table(file.path(path,"UCI HAR Dataset/train/y_train.txt"))
+temp_2 <- read.table(file.path(path,"UCI HAR Dataset/test/y_test.txt"))
+Y_data <- rbind(temp_1, temp_2)
 
 #import and combines test and train subject data
-temp_train <- read.table(file.path(path,"UCI HAR Dataset/train/subject_train.txt"))
-temp_test <- read.table(file.path(path,"UCI HAR Dataset/test/subject_test.txt"))
-Subject <- rbind(temp_train, temp_test)
+temp_1 <- read.table(file.path(path,"UCI HAR Dataset/train/subject_train.txt"))
+temp_2 <- read.table(file.path(path,"UCI HAR Dataset/test/subject_test.txt"))
+Subject_data <- rbind(temp_1, temp_2)
 
 # Merge into 1 dataset below
 
@@ -27,22 +27,22 @@ Subject <- rbind(temp_train, temp_test)
 features <- read.table(file.path(path,"UCI HAR Dataset/features.txt"))
 names(X) <- features$V2
 index_of_good_features <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
-X <- X[, index_of_good_features]
+X_data <- X_data[, index_of_good_features]
 
 # 3.Uses descriptive activity names to name the activities in the data set.
-Y$V1[Y$V1=='1']<- 'WALKING'
-Y$V1[Y$V1=='2']<- 'WALKING_UPSTAIRS'
-Y$V1[Y$V1=='3']<- 'WALKING_DOWNSTAIRS'
-Y$V1[Y$V1=='4']<- 'SITTING'
-Y$V1[Y$V1=='5']<- 'STANDING'
-Y$V1[Y$V1=='6']<- 'LAYING'
-names(Y)<- "Activity"
+Y_data$V1[Y_data$V1=='1']<- 'WALKING'
+Y_data$V1[Y_data$V1=='2']<- 'WALKING_UPSTAIRS'
+Y_data$V1[Y_data$V1=='3']<- 'WALKING_DOWNSTAIRS'
+Y_data$V1[Y_data$V1=='4']<- 'SITTING'
+Y_data$V1[Y_data$V1=='5']<- 'STANDING'
+Y_data$V1[Y_data$V1=='6']<- 'LAYING'
+names(Y_data)<- "Activity"
 
 
 # 4.Appropriately labels the data set with descriptive activity names, Merges the training and the test sets to create one data set
 
-names(Subject) <- "subjectID"
-clean <- cbind(Subject, Y, X)
+names(Subject_data) <- "subjectID"
+clean <- cbind(Subject_data, Y_data, X_data)
 write.table(clean, "merged_data.txt")
 
 # 5.Creates a 2nd, independent tidy data set with the average of each variable for each activity and each subject.
